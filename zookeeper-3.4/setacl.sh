bb660fe731845b4592b2c5561644d5bec59b6b56
Steven Borrelli <steve@aster.is>
2015-03-22 21:07:07 -0500

    Add script to set zk ACLs

---
+++ zookeeper-3.4/setacl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Sets ACLs on a zookeeper znode
+
+# Vars needed:
+# ZK_AUTH="user:password"
+# ZNODE="/myznode"
+# ZNODE_ACL="world:anyone:r,digest:user:pass:cdraw" #comma separated
+# ZK_SERVER="zookeeper server:port"
+
+ZK_SERVER=${ZK_SERVER:-"localhost:2181"}
+
+printf "addauth digest ${ZK_AUTH}\nsetAcl ${ZNODE} ${ZNODE_ACL}\n" | /opt/zookeeper/bin/zkCli.sh -server ${ZK_SERVER}
